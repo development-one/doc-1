25dbc2b6b125e08f66aef3c3bd51fca069548791
coder123 <coder123@163.com>
2020-11-12 18:51:34 +0800

    master

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    themeConfig: {
+        logo: '/assets/img/app.png',
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'About', link: '/about'},
+        ]
+      }
+  }
