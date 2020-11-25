4b62e1f2625871abc026a8890757be276bcf048e
coder123 <coder123@163.com>
2020-11-25 20:31:47 +0800

    doc1-update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,29 @@
 module.exports = {
-    themeConfig: {
-        logo: '/assets/img/app.png',
-        nav: [
-          { text: 'Home', link: '/' },
-          { text: 'About', link: '/about'},
-        ]
-      }
+  title: "飞跃大山与大海",
+  description: "无敌的寂寞",
+  lastUpdated: '更新时间',
+  head: [
+    ['link', { rel: 'icon', href: '/app.ico' }]
+  ],
+  themeConfig: {
+    logo: '/assets/img/app.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/foo/' },
+    ],
+    sidebar: {
+      '/foo/': [
+        '',     /* /foo/ */
+        'one',  /* /foo/one.html */
+        'two'   /* /foo/two.html */
+      ],
+
+      '/bar/': [
+        '',      /* /bar/ */
+        'one', /* /bar/three.html */
+        'two'   /* /bar/four.html */
+      ],
+
+    }
   }
+}
---
+++ docs/foo/one.md
@@ -0,0 +1,4 @@
+---
+title: "asdsd"
+---
+# eeeeee
